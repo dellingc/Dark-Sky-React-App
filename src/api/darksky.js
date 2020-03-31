0a618288ff0e8eac19807754ce46628adddb1e29
dellingc <35514357+dellingc@users.noreply.github.com>
2020-03-31 16:13:20 -0400

    updated api endpoint

--- src/api/darksky.js
+++ src/api/darksky.js
@@ -4,5 +4,5 @@ const devURL = 'http://localhost:8000/loc'
 const prodURL = 'https://weather-redis-app.herokuapp.com/loc'
 
 export default axios.create({
-    baseURL: devURL
+    baseURL: prodURL
 })
