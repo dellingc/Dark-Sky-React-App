1ed5b2dc96b0e3a6cf25ea6f6e6753c6d900d312
dellingc <35514357+dellingc@users.noreply.github.com>
2020-03-31 16:10:09 -0400

    added locations

--- src/components/Location.js
+++ src/components/Location.js
@@ -43,15 +43,22 @@ class Location extends React.Component {
             <select id='location'>
                 <option value=''>---</option>
                 <option value='?lat=30.2547&lon=-97.7473'>Austin, TX</option>
+                <option value='?lat=33.3061&lon=44.3650'>Baghdad, IRQ</option>
                 <option value='?lat=41.8559&lon=-87.6310'>Chicago, IL</option>
+                <option value='?lat=23.1182&lon=-82.3673'>Havana, CUB</option>
+                <option value='?lat=21.3018&lon=-157.8573'>Honolulu, HI</option>
+                <option value='?lat=8.7287&lon=167.7396'>Kwajalein, RMI</option>
                 <option value='?lat=36.0840&lon=-115.1646'>Las Vegas, NV</option>
                 <option value='?lat=25.7659&lon=-80.2149'>Miami, FL</option>
+                <option value='?lat=35.9268&lon=-75.6108'>Nags Head, NC</option>
                 <option value='?lat=29.9416&lon=-90.0834'>New Orleans, LA</option>
                 <option value='?lat=40.7456&lon=-73.9881'>New York, NY</option>
+                <option value='?lat=48.8670&lon=2.3480'>Paris, FRN</option>
                 <option value='?lat=37.5260&lon=-77.4416'>Richmond, VA</option>
                 <option value='?lat=-22.9357&lon=-43.1948'>Rio de Janeiro, BRA</option>
                 <option value='?lat=43.158&lon=-77.5996'>Rochester, NY</option>
                 <option value='?lat=37.7950&lon=-122.4190'>San Francisco, CA</option>
+                <option value='?lat=39.0858&lon=-78.1910'>Stephens City, VA</option>
                 <option value='?lat=-33.8791&lon=151.1029'>Sydney, AUS</option>
                 <option value='?lat=35.6820&lon=139.7575'>Tokyo, JPN</option>
                 <option value='?lat=38.8896&lon=-77.0344'>Washington, DC</option>
